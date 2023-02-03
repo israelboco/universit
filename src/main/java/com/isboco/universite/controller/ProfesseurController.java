4bf42fe0c663ffb2fefadb994e88f8a58a50f9fd
BOCO Israël <isboco1@gmail.com>
2023-02-03 15:20:32 +0100

    create function getmapping in professeur

--- src/main/java/com/isboco/universite/controller/ProfesseurController.java
+++ src/main/java/com/isboco/universite/controller/ProfesseurController.java
@@ -1,5 +1,21 @@
 package com.isboco.universite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.isboco.universite.model.Professeur;
+import com.isboco.universite.service.ProfesseurService;
+
+@RestController
 public class ProfesseurController {
+	
+	@Autowired
+	private ProfesseurService professeurService;
+	
+	@GetMapping("/professeurs")
+	public Iterable<Professeur> getProfesseur(){
+		return professeurService.getProfesseur();
+	}
 
 }
