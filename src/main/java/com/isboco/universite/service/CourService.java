090f681663bc2f407c740bf3626eb9d4f12fefdd
BOCO Israël <isboco1@gmail.com>
2023-02-03 14:52:26 +0100

    create service cour and etudiant

--- src/main/java/com/isboco/universite/service/CourService.java
+++ src/main/java/com/isboco/universite/service/CourService.java
@@ -1,5 +1,34 @@
 package com.isboco.universite.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isboco.universite.model.Cour;
+import com.isboco.universite.repository.CourRepository;
+
+@Service
 public class CourService {
+	
+	@Autowired
+	private CourRepository courRepository;
+	
+	public Optional<Cour> getCour(final Long id) {
+		return courRepository.findById(id);
+	}
+	
+	public Iterable<Cour> getCour(){
+		return courRepository.findAll()
+	}
+	
+	public void deleteCour(final Long id) {
+		courRepository.deleteById(id);
+	}
+	
+	public Cour saveCour(Cour c) {
+		Cour savedCour = courRepository.save(c);
+		return savedCour;
+	}
 
 }
--- src/main/java/com/isboco/universite/service/EtudiantService.java
+++ src/main/java/com/isboco/universite/service/EtudiantService.java
@@ -1,5 +1,35 @@
 package com.isboco.universite.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isboco.universite.model.Etudiant;
+import com.isboco.universite.repository.EtudiantRepository;
+
+@Service
 public class EtudiantService {
+	
+	@Autowired
+	private EtudiantRepository etudiantRepository;
+	
+	public Optional<Etudiant> getEtudiant(final Long id){
+		return etudiantRepository.findById(id);
+	}
+	
+	public Iterable<Etudiant> getEtudiant(){
+		return etudiantRepository.findAll();
+	}
+	
+	public void deleteEtudiant(final Long id) {
+		etudiantRepository.deleteById(id);
+	}
+	
+	public Etudiant saveEtudiant(Etudiant e) {
+		Etudiant savedEtudiant = etudiantRepository.save(e);
+		return savedEtudiant;
+	}
+	
 
 }
